17730e53f16720419bace1088e29bb9fa171beff
kentif <168837053+kentif@users.noreply.github.com>
2024-05-06 09:26:09 -0400

    Day 1 Commit
    I created a background for my game and rescaled to fit in the window.

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-Create initial repository 
+import pygame
+
+pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Arial', 20)
+pygame.display.set_caption("1v1")
+
+size = (1138, 600)
+screen = pygame.display.set_mode(size)
+
+bg = pygame.image.load("background.png")
+
+title_screen = True
+run = True
+# -------- Main Program Loop -----------
+frame = 0
+while run:
+    # --- Main event loop
+    if title_screen:
+        screen.blit(bg, (0, 0))
+        pygame.display.update()
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                run = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                title_screen = False
+    else:
+        screen.blit(bg, (0, 0))
+
+pygame.quit()
