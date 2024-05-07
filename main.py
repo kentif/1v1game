f0e20bff3020adabe3f3bf634c554227ac96beda
kentif <168837053+kentif@users.noreply.github.com>
2024-05-07 09:27:08 -0400

    Day 2
    I finished my menu screen and found some sprites for my player 1.

--- main.py
+++ main.py
@@ -12,12 +12,24 @@ bg = pygame.image.load("background.png")
 
 title_screen = True
 run = True
+
+display_name = my_font.render("Welcome to 1v1", True, (235, 52, 52))
+display_instructions_1 = my_font.render("Player 1: WASD for movement  C to jump E to shoot", True, (235, 52, 52))
+display_instructions_2 = my_font.render("Player 2: IJKL for movement  . to jump O to shoot", True, (235, 52, 52))
+display_rule = my_font.render("Kill the enemy player first to win ", True, (235, 52, 52))
+display_start = my_font.render("Click anywhere to start", True, (235, 52, 52))
+
 # -------- Main Program Loop -----------
 frame = 0
 while run:
     # --- Main event loop
     if title_screen:
         screen.blit(bg, (0, 0))
+        screen.blit(display_name, (330, 200))
+        screen.blit(display_instructions_1, (330, 230))
+        screen.blit(display_instructions_2, (330, 250))
+        screen.blit(display_rule, (330, 270))
+        screen.blit(display_start, (330, 290))
         pygame.display.update()
         for event in pygame.event.get():  # User did something
             if event.type == pygame.QUIT:  # If user clicked close
@@ -26,5 +38,9 @@ while run:
                 title_screen = False
     else:
         screen.blit(bg, (0, 0))
+        pygame.display.update()
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                run = False
 
 pygame.quit()
